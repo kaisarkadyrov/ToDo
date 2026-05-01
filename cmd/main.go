1854bd0d192e80ed45960522ce73a906597dd645
Kaisar Kadyrov <usernewuser0614@gmail.com>
2026-05-01 20:34:37 +0500

    Enhanced Print

--- cmd/main.go
+++ cmd/main.go
@@ -11,7 +11,10 @@ func main() {
 	todos.Add("Buy milk")
 	todos.Add("Read a book")
 	fmt.Println(todos)
-	todos.Delete(1)
+	todos.Toggle(1)
+	// todos.Toggle(1)
+	todos.Edit(0, "Buy book")
 	fmt.Println(todos)
 
+	todos.Print()
 }
--- internal/handlers/todo.go
+++ internal/handlers/todo.go
@@ -3,7 +3,10 @@ package todo_handlers
 import (
 	"errors"
 	"fmt"
+	"os"
 	"time"
+
+	"github.com/olekukonko/tablewriter"
 )
 
 type Todo struct {
@@ -45,4 +48,57 @@ func (t *Todos) Delete(index int) error {
 
 	*t = append(todos[:index], todos[index+1:]...)
 	return nil
+}
+
+func (t *Todos) Toggle(index int) error {
+	todos := *t
+
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+
+	isComplited := todos[index].Completed
+
+	if !isComplited {
+		completionTime := time.Now()
+		todos[index].FinishedAt = &completionTime
+	}
+
+	todos[index].Completed = !isComplited
+
+	return nil
+}
+
+func (t *Todos) Edit(index int, title string) error {
+	todos := *t
+
+	if err := todos.validateIndex(index); err != nil {
+		return err
+	}
+
+	todos[index].Title = title
+
+	return nil
+}
+
+func (todos Todos) Print() {
+	data := []string{"Title", "Completed", "CreatedAt", "FinishedAt"}
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.Header(data)
+
+	for _, t := range todos {
+		finishedAt := "—"
+		if t.FinishedAt != nil {
+			finishedAt = t.FinishedAt.Format("2006-01-02 15:04:05")
+		}
+		table.Append([]string{
+			t.Title,
+			fmt.Sprintf("%v", t.Completed),
+			t.CreatedAt.Format("2006-01-02 15:04:05"),
+			finishedAt,
+		})
+	}
+
+	table.Render()
 }
